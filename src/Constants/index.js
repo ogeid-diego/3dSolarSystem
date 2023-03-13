20922edbd02b7f7cca17bf4dc1c3b735300c3099
DiegoCunha <diego.cunha@itriad.org.br>
2023-03-13 14:38:49 -0400

    Uploading code already finished

---
+++ src/Constants/index.js
@@ -0,0 +1,75 @@
+export const planets = [
+  {
+    size: 3.2,
+    texture: "./src/assets/mercury.jpg",
+    position: 28,
+    sunRotSpeed: 0.04,
+    rotationSpeed: 0.004
+  },
+  {
+    size: 5.8,
+    texture: "./src/assets/venus.jpg",
+    position: 44,
+    sunRotSpeed: 0.015,
+    rotationSpeed: 0.004
+  },
+  {
+    size: 6,
+    texture: "./src/assets/earth.jpg",
+    position: 62,
+    sunRotSpeed: 0.01,
+    rotationSpeed: 0.004
+  },
+  {
+    size: 4,
+    texture: "./src/assets/mars.jpg",
+    position: 78,
+    sunRotSpeed: 0.008,
+    rotationSpeed: 0.004
+  },
+  {
+    size: 12,
+    texture: "./src/assets/jupiter.jpg",
+    position: 100,
+    sunRotSpeed: 0.002,
+    rotationSpeed: 0.004
+  },
+  {
+    size: 10,
+    texture: "./src/assets/saturn.jpg",
+    position: 138,
+    sunRotSpeed: 0.0009,
+    rotationSpeed: 0.004,
+    ring: {
+      innerRadius: 10, 
+      outerRadius: 20, 
+      texture: "./src/assets/saturnRing.png"
+    }
+  },
+  {
+    size: 3.2,
+    texture: "./src/assets/uranus.jpg",
+    position: 176,
+    sunRotSpeed: 0.0004,
+    rotationSpeed: 0.004,
+    ring: {
+      innerRadius: 7, 
+      outerRadius: 12, 
+      texture: "./src/assets/uranusRing.png"
+    }
+  },
+  {
+    size: 7,
+    texture: "./src/assets/neptune.jpg",
+    position: 200,
+    sunRotSpeed: 0.0001,
+    rotationSpeed: 0.004
+  },
+  {
+    size: 2.8,
+    texture: "./src/assets/pluto.jpg",
+    position: 216,
+    sunRotSpeed: 0.0007,
+    rotationSpeed: 0.004
+  },
+]
